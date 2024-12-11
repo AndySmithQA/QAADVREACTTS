ca931b7c0d436145a8601a4debe7d363c86488b5
AndySmithQA <andy.smith@qa.com>
2024-12-11 09:45:36 +0000

    RenderProps content

---
+++ Day 5/renderprops/src/Kelvin.tsx
@@ -0,0 +1,10 @@
+interface KelvinProps {
+  value: number;
+}
+
+function Kelvin({ value }: KelvinProps) {
+    return <div className="temp">{parseInt(value.toString() || '0') + 273.15}K</div>;
+  }
+
+export default Kelvin;
+
